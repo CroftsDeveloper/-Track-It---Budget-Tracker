94906fed5ab123e87be6bbe4ead749deef58963c
CroftsDeveloper <crofts.webdeveloper@gmail.com>
2023-08-19 14:51:55 +0100

    Added summary section - income, expenses & balance

--- script.js
+++ script.js
@@ -2,16 +2,18 @@
 document.getElementById('add-income-form').addEventListener('submit', function(e) {
     e.preventDefault();
     const name = document.getElementById('income-name').value;
-    const amount = document.getElementById('income-amount').value;
+    const amount = parseFloat(document.getElementById('income-amount').value);
     addIncome(name, amount); // Call addIncome function
+    updateSummary(); // Update the summary
 });
 
 // Event listener to handle expense form submission
 document.getElementById('add-expense-form').addEventListener('submit', function(e) {
     e.preventDefault();
     const name = document.getElementById('expense-name').value;
-    const amount = document.getElementById('expense-amount').value;
+    const amount = parseFloat(document.getElementById('expense-amount').value);
     addExpense(name, amount); // Call addExpense function
+    updateSummary(); // Update the summary
 });
 
 // Function to add income to the list and delete button functionality
@@ -22,6 +24,7 @@ function addIncome(name, amount) {
     incomeList.appendChild(li);
     li.querySelector('.delete-button').addEventListener('click', function() {
         incomeList.removeChild(li); // Remove list item on delete button click
+        updateSummary(); // Update the summary
     });
 }
 
@@ -33,5 +36,30 @@ function addExpense(name, amount) {
     expenseList.appendChild(li);
     li.querySelector('.delete-button').addEventListener('click', function() {
         expenseList.removeChild(li); // Remove list item on delete button click
+        updateSummary(); // Update the summary
     });
+}
+
+// Function to update the summary section with total income, expenses, and balance
+function updateSummary() {
+    let totalIncome = 0;
+    let totalExpenses = 0;
+
+    // Calculate total income
+    document.querySelectorAll('#income-list li').forEach(item => {
+        totalIncome += parseFloat(item.textContent.split(' ')[3].substring(1));
+    });
+
+    // Calculate total expenses
+    document.querySelectorAll('#expense-list li').forEach(item => {
+        totalExpenses += parseFloat(item.textContent.split(' ')[3].substring(1));
+    });
+
+    // Calculate balance
+    const balance = totalIncome - totalExpenses;
+
+    // Update the summary section
+    document.getElementById('total-income').textContent = `£${totalIncome.toFixed(2)}`;
+    document.getElementById('total-expenses').textContent = `£${totalExpenses.toFixed(2)}`;
+    document.getElementById('balance').textContent = `£${balance.toFixed(2)}`;
 }
