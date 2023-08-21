80a70a3fea3bd1671aa2d580e3754d210f340a83
CroftsDeveloper <crofts.webdeveloper@gmail.com>
2023-08-21 21:23:31 +0100

    re-do Expense Tracker functionality and UI enhancements
    - Made large change to the HTML and JS structure and layout as was not functioning as first intended
    -  re-did the "Reset Tracker" button functionality
    - Added better comments throughout the code to improve code readability and maintainability.
    - Refactored and optimised JavaScript functions for adding income and expenses, editing items, and calculating totals.
    - Local storage implemented
    - CSS styling finalised
    - Set GBP as the default currency in the HTML `select` element
    - Modified the `formatCurrency` function

--- script.js
+++ script.js
@@ -1,89 +1,206 @@
-// Event listener to handle income form submission
-document.getElementById('add-income-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const name = document.getElementById('income-name').value;
-    const amount = parseFloat(document.getElementById('income-amount').value);
-    addIncome(name, amount);
-    updateSummary();
-});
+(function () {
+    // Mapping between currency codes and symbols
+    const currencySymbols = {
+        'GBP': '£',
+        'EUR': '€',
+        'USD': '$'
+    };
 
-// Event listener to handle expense form submission
-document.getElementById('add-expense-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const name = document.getElementById('expense-name').value;
-    const amount = parseFloat(document.getElementById('expense-amount').value);
-    addExpense(name, amount);
-    updateSummary();
-});
-
-// Function to add income
-function addIncome(name, amount) {
-    const incomeList = document.getElementById('income-list');
-    const li = createListItem(name, amount);
-    incomeList.appendChild(li);
-}
-
-// Function to add expense
-function addExpense(name, amount) {
-    const expenseList = document.getElementById('expense-list');
-    const li = createListItem(name, amount);
-    expenseList.appendChild(li);
-}
-
-function createListItem(name, amount) {
-    const li = document.createElement('li');
-    li.innerHTML = `Name: ${name}, Amount: ${formatCurrency(amount)} <button class="edit-button">Edit</button> <button class="delete-button">Delete</button>`;
-    
-    li.querySelector('.edit-button').addEventListener('click', function() {
-        const newName = prompt("Edit name:", name);
-        const newAmount = parseFloat(prompt("Edit amount:", amount));
-        li.innerHTML = `Name: ${newName}, Amount: ${formatCurrency(newAmount)} <button class="edit-button">Edit</button> <button class="delete-button">Delete</button>`;
-        updateSummary();
+    // Event listener for income form submission
+    document.getElementById('income-form').addEventListener('submit', function (e) {
+        e.preventDefault();
+        handleFormSubmission('income');
     });
 
-    li.querySelector('.delete-button').addEventListener('click', function() {
-        li.parentNode.removeChild(li);
-        updateSummary();
+    // Event listener for expense form submission
+    document.getElementById('expense-form').addEventListener('submit', function (e) {
+        e.preventDefault();
+        handleFormSubmission('expense');
     });
 
-    return li;
-}
-
-// Function to format currency
-function formatCurrency(amount) {
-    const currencySymbol = document.getElementById('currency').value;
-    return `${currencySymbol}${amount.toFixed(2)}`;
-}
-
-// Function to update summary
-function updateSummary() {
-    let totalIncome = 0;
-    let totalExpenses = 0;
-
-    document.querySelectorAll('#income-list li').forEach(item => {
-        totalIncome += parseFloat(item.textContent.split(' ')[3].substring(1));
-    });
-
-    document.querySelectorAll('#expense-list li').forEach(item => {
-        totalExpenses += parseFloat(item.textContent.split(' ')[3].substring(1));
-    });
-
-    const balance = totalIncome - totalExpenses;
-
-    document.getElementById('total-income').textContent = formatCurrency(totalIncome);
-    document.getElementById('total-expenses').textContent = formatCurrency(totalExpenses);
-    document.getElementById('balance').textContent = formatCurrency(balance);
-}
-
-// Event listener for delete all button
-document.getElementById('delete-all-btn').addEventListener('click', function() {
-    const confirmation = confirm('Are you sure you want to delete all income and expenses? This action cannot be undone.');
-    if (confirmation) {
-        document.getElementById('income-list').innerHTML = '';
-        document.getElementById('expense-list').innerHTML = '';
-        updateSummary();
+    function handleFormSubmission(type) {
+        const description = document.getElementById(`${type}-description`).value;
+        const amount = parseFloat(document.getElementById(`${type}-amount`).value);
+        type === 'income' ? addIncome(description, amount) : addExpense(description, amount);
+        calculateTotal();
+        this.reset();
+        saveToLocalStorage();
     }
-});
 
-// Event listener for currency selection
-document.getElementById('currency').addEventListener('change', updateSummary);
+    // Function to add income
+    function addIncome(description, amount) {
+        const incomeList = document.getElementById('income-list');
+        const li = createListItem(description, amount, 'income');
+        incomeList.appendChild(li);
+    }
+
+    // Function to add expense
+    function addExpense(description, amount) {
+        const expenseList = document.getElementById('expense-list');
+        const li = createListItem(description, amount, 'expense');
+        expenseList.appendChild(li);
+    }
+
+    // Function to create list item
+    function createListItem(description, amount, type) {
+        const li = document.createElement('li');
+        li.dataset.type = type;
+        li.dataset.amount = amount;
+
+        const descriptionDiv = document.createElement('div');
+        descriptionDiv.className = 'description';
+        descriptionDiv.textContent = description;
+
+        const amountDiv = document.createElement('div');
+        amountDiv.className = 'amount';
+        amountDiv.textContent = formatCurrency(amount);
+
+        const actionsDiv = document.createElement('div');
+        actionsDiv.className = 'actions';
+
+        const editButton = createActionButton('Edit', function () {
+            editItem(li, descriptionDiv, amountDiv, type);
+        }, 'Edit item');
+        const deleteButton = createActionButton('Delete', function () {
+            li.remove();
+            calculateTotal();
+            saveToLocalStorage();
+        }, 'Delete item');
+
+        actionsDiv.appendChild(editButton);
+        actionsDiv.appendChild(deleteButton);
+
+        li.appendChild(descriptionDiv);
+        li.appendChild(amountDiv);
+        li.appendChild(actionsDiv);
+
+        return li;
+    }
+
+    // Function to create action button
+    function createActionButton(text, onClick, ariaLabel) {
+        const button = document.createElement('button');
+        button.textContent = text;
+        button.addEventListener('click', onClick);
+        button.setAttribute('aria-label', ariaLabel);
+        return button;
+    }
+
+    // Function to edit list item
+    function editItem(listItem, descriptionDiv, amountDiv, type) {
+        const form = document.getElementById(`${type}-form`);
+        const descriptionInput = form.querySelector(`#${type}-description`);
+        const amountInput = form.querySelector(`#${type}-amount`);
+        const addButton = form.querySelector('.add-btn');
+        const updateButton = form.querySelector('.update-btn');
+
+        descriptionInput.value = descriptionDiv.textContent;
+        amountInput.value = parseFloat(amountDiv.textContent.substring(1));
+
+        addButton.hidden = true;
+        updateButton.hidden = false;
+
+        updateButton.onclick = function () {
+            if (descriptionInput.value !== '' && amountInput.value !== '') {
+                const newAmount = parseFloat(amountInput.value);
+                descriptionDiv.textContent = descriptionInput.value;
+                amountDiv.textContent = formatCurrency(newAmount);
+                listItem.dataset.amount = newAmount;
+                calculateTotal();
+                saveToLocalStorage();
+
+                form.reset();
+                addButton.hidden = false;
+                updateButton.hidden = true;
+            }
+        };
+    }
+    
+    // Function to format currency
+function formatCurrency(amount) {
+    const currencySelect = document.getElementById('currency');
+    const selectedOption = currencySelect.options[currencySelect.selectedIndex];
+    const currencyCode = selectedOption.value || 'GBP'; // Default to GBP if not set
+    const currencySymbol = currencySymbols[currencyCode];
+    return currencySymbol + amount.toFixed(2);
+}
+
+    // Function to calculate total income, expenses, and balance
+    function calculateTotal() {
+        const incomeListItems = document.querySelectorAll('#income-list li');
+        const expenseListItems = document.querySelectorAll('#expense-list li');
+
+        let totalIncome = 0;
+        let totalExpenses = 0;
+
+        incomeListItems.forEach((item) => {
+            totalIncome += parseFloat(item.dataset.amount);
+        });
+
+        expenseListItems.forEach((item) => {
+            totalExpenses += parseFloat(item.dataset.amount);
+        });
+
+        const balance = totalIncome - totalExpenses;
+        const balanceElement = document.getElementById('balance');
+        balanceElement.textContent = formatCurrency(balance);
+
+        if (balance < 0) {
+            balanceElement.style.color = 'red';
+        } else {
+            balanceElement.style.color = 'inherit';
+        }
+
+        document.getElementById('total-income').textContent = formatCurrency(totalIncome);
+        document.getElementById('total-expenses').textContent = formatCurrency(totalExpenses);
+    }
+
+    // Function to save to local storage
+    function saveToLocalStorage() {
+        const incomeData = [];
+        document.querySelectorAll('#income-list li').forEach((li) => {
+            incomeData.push({ description: li.querySelector('.description').textContent, amount: li.dataset.amount });
+        });
+        const expenseData = [];
+        document.querySelectorAll('#expense-list li').forEach((li) => {
+            expenseData.push({ description: li.querySelector('.description').textContent, amount: li.dataset.amount });
+        });
+        localStorage.setItem('income', JSON.stringify(incomeData));
+        localStorage.setItem('expenses', JSON.stringify(expenseData));
+    }
+
+    // Function to load from local storage
+    function loadFromLocalStorage() {
+        const incomeData = JSON.parse(localStorage.getItem('income'));
+        const expenseData = JSON.parse(localStorage.getItem('expenses'));
+        if (incomeData) {
+            incomeData.forEach((item) => {
+                addIncome(item.description, parseFloat(item.amount));
+            });
+        }
+        if (expenseData) {
+            expenseData.forEach((item) => {
+                addExpense(item.description, parseFloat(item.amount));
+            });
+        }
+        calculateTotal();
+    }
+
+    // Event listener for currency change
+    document.getElementById('currency').addEventListener('change', calculateTotal);
+
+    // Event listener for reset tracker button
+    document.getElementById('reset-tracker').addEventListener('click', function () {
+        const confirmation = confirm('Are you sure you want to reset the tracker? This action cannot be undone.');
+        if (confirmation) {
+            document.getElementById('income-list').innerHTML = '';
+            document.getElementById('expense-list').innerHTML = '';
+            localStorage.removeItem('income');
+            localStorage.removeItem('expenses');
+            calculateTotal();
+        }
+    });
+
+    // Load data from local storage on page load
+    loadFromLocalStorage();
+})();
